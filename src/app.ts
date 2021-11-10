a83b19ed94f33ed27365d0c04d80ee3636947586
samson akande <akandesamson12@gmail.com>
2021-11-10 14:21:16 +0100

    change DB connection type to URL

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { BASE_PATH, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, ENVIRONMENT } from './config';
+import { BASE_PATH, DATABASE_URL, ENVIRONMENT } from './config';
 import { errorHandler, global } from './middlewares';
 import { logger } from './utils';
 import { createConnection } from 'typeorm';
@@ -36,12 +36,8 @@ class App {
             try {
                 await createConnection({
                     type: 'postgres',
-                    host: DB_HOST,
-                    port: 5432,
-                    username: DB_USER,
-                    password: DB_PASSWORD,
-                    database: DB_NAME,
-                    synchronize: true,
+                    url: DATABASE_URL,
+                    synchronize: false,
                     migrations: ['/src/db/migrations/*.ts'],
                     entities: [__dirname + '/api/**/*Model.js'],
                 });
@@ -77,5 +73,3 @@ class App {
 
 const app = new App().express;
 export default app;
-
-// psql --host=ec2-54-76-249-45.eu-west-1.compute.amazonaws.com --port=5432 --username=xgkjjcsluzlsxr --password=66fdbb2316f1eb924da42f04462f8b60fc103d0284c6b8eb4e534b0483a00e78 --dbname=dek907u1dbsup0
--- src/config/index.ts
+++ src/config/index.ts
@@ -11,4 +11,5 @@ export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_HOST = process.env.DB_HOST;
+export const DATABASE_URL = process.env.DATABASE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
